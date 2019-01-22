1a5eda3769fc62fe59de61919006fb54856deded
Arun prasad k <ak207@asiapac.globalcsc.net>
2019-01-22 17:38:53 +0530

    Commit Test

---
+++ history.sh
@@ -0,0 +1,140 @@
+    1  exit
+    2  add
+    3  git add
+    4  add hello
+    5  add
+    6  git add
+    7  git init
+    8  gif add
+    9  git add hello
+   10  git add C:\Users\ak207\eclipse-workspace\HelloWorld
+   11  exit
+   12  push
+   13  git push
+   14  git remote new <ak207> <https://github.com/ak207/new.git>
+   15  git remote new ak207 https://github.com/ak207/new.git
+   16  git pull
+   17  git status
+   18  git push
+   19  git push new
+   20  git permission
+   21  git --help
+   22  git help -a
+   23  OA
+   24  ls -lrylsls
+   25  ls
+   26  vi Test.txt
+   27  git log
+   28  add .
+   29  add Test.txt
+   30  git add .
+   31  ls -lrt
+   32  git add Test.txt
+   33  ls -lrt
+   34  git commit -m "created Test.txt for sample git file"
+   35  git log
+   36  git push
+   37  git push Test.txt
+   38  git log
+   39  ls -lrt
+   40  cd eclipse-workspace
+   41  ls -lrt
+   42  dir
+   43  git branch -r
+   44  cd ..
+   45  ls -lrt
+   46  ls -lrt
+   47  git add .
+   48  edit
+   49  git edit
+   50  git init
+   51  git clone /c/users/ak207/gitrepo/testrepo.git
+   52  git init test.git
+   53  git clone /c/users/ak207/gitrepo/testrepo.git
+   54  cd test
+   55  ls -lrt
+   56  cd test.git
+   57  ls
+   58  git log
+   59  cd..
+   60  git --version
+   61  mkdir edureka-repo
+   62  cd edureka-repo
+   63  git init
+   64  touch edureka.py
+   65  gedit edureka.py
+   66  gedit
+   67  edit
+   68  git init
+   69  git edit edureka.py
+   70  git init edureka.py
+   71  git init
+   72  git add edureka.py
+   73  git add webpage.html
+   74  touch webpage.html
+   75  git touch webpage.html
+   76  touch webpage.html
+   77  edit
+   78  edit webpage.html
+   79  webpage.html
+   80  git edit
+   81  > webpage.html
+   82  echo " " >webpage.html
+   83  git add webpage.html
+   84  ./webpage.html
+   85  git create index.html
+   86  git reset index.html
+   87  git create index.html
+   88  ls
+   89  touch index.html
+   90  ls
+   91  ls -lrt
+   92  echo " " >webpage.html -lrt
+   93  git add webpage.html
+   94  git add webpage.html -lrt
+   95  git config --edit
+   96  git webpage.html --edit
+   97  git config --edit webpage.html
+   98  git --edit webpage.html
+   99  start webpage.html
+  100  start webpage.html -lrt
+  101  git start webpage.html
+  102  git stage
+  103  git stash
+  104  git commit webpage.html
+  105  git commit
+  106  notepad .gitignore
+  107  notepad webpage.html
+  108  ./webpage.html
+  109  notepad webpage.html
+  110  ./webpage.html
+  111  git edureka.py
+  112  git add edureka.py
+  113  git add .
+  114  touch webpage.html
+  115  gedit
+  116  git add.
+  117  git add .
+  118  git add .
+  119  ls -lrt
+  120  ./edureka.py
+  121  git delete
+  122  git add .
+  123  touch hi.txt
+  124  touch hello.txt
+  125  git commit
+  126  git commit -m "Commit everything"
+  127  git add .
+  128  git stash
+  129  git log
+  130  git status
+  131  execute.sh
+  132  ./execute.sh
+  133  touch new.html
+  134  ./execute.sh
+  135  git status
+  136  ./execute.sh
+  137  git pull
+  138  git status
+  139  ./execute.sh
+  140  history >history.sh
